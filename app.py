a265159a89b4374892e4f77755270165b8c8ae2f
Your Name <you@example.com>
2024-01-12 14:55:18 +0530

    version 1.1

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/weatherapp', methods = ["POST"])
+def weatherapp():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': request.form.get("city"),
+        'units' : request.form.get("units"),
+        'appid' : request.form.get("appid")
+    }
+    data = requests.get(url, params)
+    datajson = data.json()
+    return datajson
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
